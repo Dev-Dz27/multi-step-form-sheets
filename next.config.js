57ae0515ab66fbc6449131ce610f805be7c7a2eb
Chekkal Youcef <chekkalislam@gmail.com>
2024-02-18 20:08:57 +0100

    fix(next.config): removed the experimental appDir flag

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  // experimental: {
+  //   appDir: true,
+  // },
+};
